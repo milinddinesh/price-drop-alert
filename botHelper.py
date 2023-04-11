6aa56fb3c91915e2df581b2d10d26ce16eeaf643
Milind <milinddinesh@gmail.com>
2023-04-11 23:17:18 +0530

    add save()

--- botHelper.py
+++ botHelper.py
@@ -1,10 +1,27 @@
 import json
+import crawler
+import os
 
-def save(user , item , price):
-    file = open("jsonData.json", "a")
-    data = json.load(file)
-    #add write function to json file 
-    #figure out way to create a json array in the file 
-    #figure out a way to append data to the json array in the file.
+FILE_NAME = "jsonData.json"
+
+
     #call this function from the track function 
     #return the appropriate response
+def save(user, item , url):
+    data = []
+    with open(FILE_NAME,"r") as file :
+        try:
+            print("inside try")
+            data = json.load(file)
+        except json.JSONDecodeError:
+            pass
+
+    price = crawler.get_price(url)
+    item_data = {"user":user,"item":item,"price":price,"url":url}
+    data.append(item_data)
+    print(data)
+    with open(FILE_NAME,"w") as file :
+        json.dump(data,file)
+    return price
+
+
